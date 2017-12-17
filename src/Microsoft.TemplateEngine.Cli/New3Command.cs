264271f63a6e4ccea2753b47bbca87917729ea31
Dan Walmsley <dan@walms.co.uk>
2017-12-17 20:53:35 +0000

    mods for AvalonStudio.

--- src/Microsoft.TemplateEngine.Cli/New3Command.cs
+++ src/Microsoft.TemplateEngine.Cli/New3Command.cs
@@ -712,7 +712,7 @@ namespace Microsoft.TemplateEngine.Cli
         {
             Reporter.Output.WriteLine(LocalizableStrings.CommandDescription);
             Reporter.Output.WriteLine();
-            Reporter.Output.WriteLine(string.Format(LocalizableStrings.Version, GitInfo.PackageVersion));
+            //Reporter.Output.WriteLine(string.Format(LocalizableStrings.Version, GitInfo.PackageVersion));
             Reporter.Output.WriteLine(string.Format(LocalizableStrings.CommitHash, GitInfo.CommitHash));
         }
 
